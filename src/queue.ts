5c8de412688228aaa33af8c37fe1fc844c378ba9
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2024-01-23 19:03:29 +0000

    Add Azure Queue Util (#1)
    * add azure queue util
    
    * Add error handling for missing connection string and queue name

---
+++ src/queue.ts
@@ -0,0 +1,30 @@
+import { QueueClient } from "@azure/storage-queue"
+
+export class AzureQueue {
+  private queueClient: QueueClient
+
+  constructor(connectionString: string, queueName: string) {
+    if (!connectionString) {
+      throw new Error("Missing connection string")
+    }
+    if (!queueName) {
+      throw new Error("Missing queue name")
+    }
+    this.queueClient = new QueueClient(connectionString, queueName)
+  }
+
+  /**
+   * @returns {QueueClient} - A QueueClient object
+   */
+  getQueueClient(): QueueClient {
+    return this.queueClient
+  }
+
+  /**
+   * @param {string} message - The message to send
+   * @returns {Promise<any>} - The response from the queue
+   */
+  async sendMessage(message: string): Promise<any> {
+    return this.queueClient.sendMessage(message)
+  }
+}
